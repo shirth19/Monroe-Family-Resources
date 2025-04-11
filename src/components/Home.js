65a7869a6673c0a444eb3912e30db21a280ca8b9
shirth19 <sam.hirth19@gmail.com>
2025-04-11 16:33:17 -0400

    Fix: Remove unused imports to resolve build warnings

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 // Import family-friendly icons
-import { FaHome, FaHandHoldingHeart, FaHospital, FaBrain, FaHouseUser, FaHandsHelping, FaChild } from 'react-icons/fa';
+import { FaHandHoldingHeart, FaHospital, FaBrain, FaHouseUser, FaHandsHelping, FaChild } from 'react-icons/fa';
 
 function Home() {
   return (
